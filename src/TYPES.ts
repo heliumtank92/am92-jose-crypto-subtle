12a90ac51f4ebb136e4604ba18f61b4eb649d721
Mehul Agarwal <agarwalmehul92@gmail.com>
2023-06-15 20:06:08 +0530

    Fix Description for types

--- src/TYPES.ts
+++ src/TYPES.ts
@@ -1,8 +1,18 @@
+/**
+ * Type defination for the keys returned from generateAndWrapKey function
+ *
+ * @typedef {JoseCryptoKeyObj}
+ */
 export type JoseCryptoKeyObj = {
   encryptionKey: CryptoKey
   encryptedEncryptionKey: string
 }
 
+/**
+ * Type defination for error map to be passed to JoseCryptoSubtleError.
+ *
+ * @typedef {JoseCryptoSubtleErrorMap}
+ */
 export type JoseCryptoSubtleErrorMap = {
   message: string
   errorCode: string
